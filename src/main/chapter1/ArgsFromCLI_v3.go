92260854d52a9e0ea3ec4521987581789e1b5e7c
Rob Evans <robevansuk@users.noreply.github.com>
2022-11-04 22:48:45 +0000

    add more concise version of ArgsFromCLI_v3.go

---
+++ src/main/chapter1/ArgsFromCLI_v3.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+/**
+ * This application replicates the echo command in the Unix terminal
+ * by returning whatever args are passed to it
+ * Usage:
+ * go run ArgsFromCLI_v3.go 123 456
+ * Outputs:
+ * 123 456
+ */
+// Implementation of Unix Echo command
+func main() {
+	// Demo the Join function from the strings package
+	fmt.Println(strings.Join(os.Args[1:], " "))
+}
