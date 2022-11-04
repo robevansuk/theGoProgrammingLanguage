d3337003f25e2000054d83fc20aef4ad31bdd5cd
Rob Evans <robevansuk@users.noreply.github.com>
2022-11-04 22:46:51 +0000

    demo different methods for assignation

---
+++ src/main/chapter1/Assignment.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	r := ""
+	fmt.Println(r)
+	// Alternatives for the same thing below
+	var s string
+	fmt.Println(s)
+	var t = ""
+	fmt.Println(t)
+	var u string = ""
+	fmt.Println(u)
+}
