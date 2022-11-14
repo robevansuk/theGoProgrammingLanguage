c11dcc44581f211c6d6d649f653cc0ce6007cab6
Rob Evans <robevansuk@users.noreply.github.com>
2022-11-14 21:23:51 +0000

    Add docs on what the example is about

--- src/main/chapter1/HttpWithChannels.go
+++ src/main/chapter1/HttpWithChannels.go
@@ -15,12 +15,15 @@ import (
  * 0.69s     55097   https://golang.org
  * 0.92s     30539   https://godoc.org
  * 1.84s      4154   http://gopl.io
+ *
+ * Channel - a communication mechanism for communicating between goroutines
+ * goroutine - new thread / concurrent function execution
  */
 func main() {
 	start := time.Now()
-	ch := make(chan string)
+	ch := make(chan string) // create a channel for strings
 	for _, url := range os.Args[1:] {
-		go fetch(url, ch) // start a go Routine
+		go fetch(url, ch) // start a go Routine - asynchronous function
 	}
 	for range os.Args[1:] {
 		fmt.Println(<-ch) // receive from channel ch
@@ -37,12 +40,12 @@ func fetch(url string, ch chan<- string) {
 		return
 	}
 
-	nbytes, err := io.Copy(ioutil.Discard, resp.Body)
+	nbytes, err := io.Copy(ioutil.Discard, resp.Body) // returns the number of bytes
 	resp.Body.Close()
 	if err != nil {
 		ch <- fmt.Sprintf("while reading %s: %v", url, err)
 		return
 	}
 	secs := time.Since(start).Seconds()
-	ch <- fmt.Sprintf("%.2fs   %7d   %s", secs, nbytes, url)
+	ch <- fmt.Sprintf("%.2fs   %7d   %s", secs, nbytes, url) // send a summary line to the channel ch
 }
