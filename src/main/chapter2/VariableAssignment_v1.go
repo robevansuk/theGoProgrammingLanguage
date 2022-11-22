bd8fa37d769cbc22de902beba8b01ba9207f5fa8
Rob Evans <robevansuk@users.noreply.github.com>
2022-11-22 21:25:30 +0000

    addition of variable declaration with reuse of a var + minimal assignment

--- src/main/chapter2/VariableAssignment_v1.go
+++ src/main/chapter2/VariableAssignment_v1.go
@@ -1,9 +1,21 @@
 package main
 
+import "fmt"
+
 func main() {
 	i, j := 3, 5
 	isValid := true
 	isNotValid := false
 	// tuple assignment - swaps j for i and i for j, and swaps isValid (true) for isNotValid (false) and vice versa.
 	i, j, isValid, isNotValid = j, i, isNotValid, isValid
+
+	in, err := someFuncReturningAValAndAnErr()
+	// Variable declaration only needs one new variable (out) - err can be reused
+	out, err := someFuncReturningAValAndAnErr()
+
+	fmt.Printf("%s, %s, %v", in, out, err)
+}
+
+func someFuncReturningAValAndAnErr() (string, error) {
+	return "", nil
 }
